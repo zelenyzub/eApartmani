27246f4de6b1094a02872f29258bd163f317db41
zelenyzub <136793603+zelenyzub@users.noreply.github.com>
2024-03-19 21:18:19 +0100

    edit user WIP

--- resources/js/components/UserAdministration.vue
+++ resources/js/components/UserAdministration.vue
@@ -76,14 +76,23 @@
 
     <!-- modal za izmenu -->
     <div class="modal" tabindex="-1" id="editUserModal">
-        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Izmeni korisnika</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="editName" class="form-label">Ime</label>
+                            <input type="text" class="form-control" id="editName">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="editSurname" class="form-label">Prezime</label>
+                            <input type="text" class="form-control" id="editSurname">
+                        </div>
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Odustani</button>
@@ -178,7 +187,7 @@ export default {
                                 '<div class="dropdown justify-content-center">' +
                                 '<a type="button" class="" data-bs-toggle="dropdown"><i class="fa-solid fa-list fa-sm" style="color: #4eb3ac;"></i></a>' +
                                 '<div class="dropdown-menu bg-dark text-center">' +
-                                '<a type="button" data-bs-toggle="modal" data-bs-target="#editUser" class="dropdown-item" data-entry-id="' +
+                                '<a type="button" data-bs-toggle="modal" data-bs-target="#editUserModal" id="editAction" class="dropdown-item disabled" data-entry-id="' +
                                 row.id +
                                 '" style="color: #4eb3ac;"><i class="fa-regular fa-pen-to-square fa-sm" style="margin-right: 5px"></i>Izmeni</a>' +
                                 '<a type="button" data-bs-toggle="modal" data-bs-target="#deleteUserModal" id="deleteAction" class=" deleteAction dropdown-item" data-entry-id="' +
