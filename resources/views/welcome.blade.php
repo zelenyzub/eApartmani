2133cba3d72d1fc09f0730ab9e485f9379db81d5
zelenyzub <136793603+zelenyzub@users.noreply.github.com>
2024-04-22 19:27:44 +0200

    welcome blade working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.app')
 @section('title', 'E-Apartmani')
 @section('content')
+<h1>Zdravoooo</h1>
 <example-component></example-component>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::middleware('auth')->group(function () {
     // KALENDAR
     Route::get('/kalendar', [App\Http\Controllers\CalendarController::class, 'calendar'])->name('kalendar');
